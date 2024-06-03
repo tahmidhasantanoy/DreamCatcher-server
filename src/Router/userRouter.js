2b6778907be017a0ff11c850c2f7753093f8c07f
nurnoby rahaman <nurnoby323@gmail.com>
2024-06-03 12:19:51 +0600

    prometed as admin and photographer

--- src/Router/userRouter.js
+++ src/Router/userRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Model/User");
 
-router.post("/user", async (req, res) => {
-    try {
-      const data =req.body
+// create user
+router.post("/register", async (req, res) => {
+  try {
+    const data = req.body;
     // data store using model
     const newUser = new User(data);
     const userData = await newUser.save();
@@ -13,4 +14,83 @@ router.post("/user", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+// find all user
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users) {
+      res.status(200).send(users);
+    } else {
+      res.status(404).send("Users not found");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// update user as photographer
+router.put("/users_to_photographer/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const userDetails = await User.findOne({ email: email });
+    if (userDetails.role != "photographer") {
+      const updateUser = await User.updateOne(
+        { email: email },
+        {
+          $set: {
+            role: "photographer",
+            promotedAt: new Date(),
+          },
+        }
+      );
+      if (updateUser) {
+        res.status(200).send({
+          success: updateUser.acknowledged,
+          message: "Promoted as a photographer",
+          data: updateUser,
+        });
+      } else {
+        res.status(404).send("User not found");
+      }
+    }
+    else {
+      res.status(404).send("User is already a photographer");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+router.put("/users_to_admin/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const userDetails = await User.findOne({ email: email });
+    if (userDetails.role != "admin") {
+      const updateUser = await User.updateOne(
+        { email: email },
+        {
+          $set: {
+            role: "admin",
+            promotedAt: new Date(),
+          },
+        }
+      );
+      if (updateUser) {
+        res.status(200).send({
+          success: updateUser.acknowledged,
+          message: "Promoted as a admin",
+          data: updateUser,
+        });
+      } else {
+        res.status(404).send("User not found");
+      }
+    }
+    else {
+      res.status(404).send("User is already a admin");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
